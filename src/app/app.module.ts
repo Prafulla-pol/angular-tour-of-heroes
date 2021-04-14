63b500c630017fc68bbcf90da8e151f45596f5dd
Prafulla Pol <prafulla.pol@sailpoint.com>
2021-04-14 16:59:52 +0530

    Seperating responsibility of the components, Input decorator and Property Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 // Every component must be declared in exactly one NgModule
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     // AppModule declares all application components,
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     // All modules are imported
